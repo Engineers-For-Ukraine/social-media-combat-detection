f9718862ebc698335c19876cdab1d185330e12e8
Jacob Moore <billingsmoore@gmail.com>
2024-04-22 13:09:22 -0400

    dockerized it

---
+++ dist/telegram/app/telegram_funcs.py
@@ -0,0 +1,36 @@
+from telethon import TelegramClient
+from datetime import timezone, timedelta, datetime
+from time import sleep
+from app.api import *
+
+# define functions to get and send messages
+async def get_messages():
+    # get AmplifyUkraine messages from last hour 
+    global api_id
+    global api_hash
+    messages = []
+    async with TelegramClient(api_id=api_id, api_hash=api_hash, session='this') as client:
+        async for message in client.iter_messages('AmplifyUkraine', offset_date=datetime.now(tz=timezone.utc) - timedelta(hours=1), reverse=True):
+            messages.append(message)
+    return messages
+
+async def post_message(message):
+    # post message to the given channel
+    # argument must be only the text of a message (i.e. message.text should be passed)
+    global api_id
+    global api_hash
+    channel = -1002016594072 # use this line for testing
+    #channel = 'CombatMessagesUA' # update this var to change channel
+    async with TelegramClient(api_id=api_id, api_hash=api_hash, session='this') as client:
+        await client.send_message(entity=channel, message=message)
+        print('Message posted')
+        print('Waiting to avoid Flood Error...')
+        sleep(5)
+
+async def post_report(report):
+    # post message of program logs
+    global api_id
+    global api_hash
+    channel = -1002008284745 # update this to private report channel
+    async with TelegramClient(api_id=api_id, api_hash=api_hash, session='this') as client:
+        await client.send_message(entity=channel, message=report)
---
+++ dist/telegram/main.py
@@ -0,0 +1,79 @@
+from app.telegram_funcs import *
+from app.model_funcs import *
+from app.map_funcs import *
+
+import asyncio
+from time import sleep
+
+async def main():
+    global classifier, vectorizer
+
+    while True:
+
+        errors = []
+        num_messages = 0
+        num_combats = 0      
+
+        current_time = datetime.now()
+
+        print(f'Running at {current_time}')  
+
+        try:
+            messages = await get_messages()
+            num_messages = len(messages)
+            print(f"{num_messages} messages downloaded")
+        except:
+            print('Failed to get messages')
+            errors.append('Failed to get messages')
+    
+    
+        if num_messages > 0:
+            try:
+                # classify messages
+                print('Classifying messages...')
+                combats = await find_combats(messages, classifier=classifier, vectorizer=vectorizer)
+                print('Messages classified')
+                num_combats = len(combats)
+                print(f"{num_combats} found")
+
+                # send combats to map
+                try:
+                    mapped = map_messages(combats)
+                    print('Messages sent to map database')
+                except:
+                    errors.append('Failed to send messages to map database')
+
+                # post combats to telegram
+                for combat in combats:
+                    try:
+                        await post_message(combat)
+                    except:
+                        errors.append('Failed to post combat messages')
+            except:
+                errors.append('Failed to classify messages')
+        
+
+        # create and post report with errors
+
+        if len(errors) == 0:
+            errors.append('None')
+
+        error_string = ''
+        for error in errors:
+            error_string += error + '\n'
+
+        report_string = f"{current_time}\nRetrieved {num_messages} new messages. \nFound {num_combats} combat events. \n{mapped} messages sent to map. \nErrors: {error_string}"
+
+        try:
+            print('Posting report...')
+            await post_report(report_string)
+        except:
+            pass
+
+        print('Done! Going to sleep for an hour...')
+
+        sleep(3600)
+
+if __name__ == "__main__":
+    classifier, vectorizer = load_model()
+    asyncio.run(main())
